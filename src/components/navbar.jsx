206779cdfcb57e43b6b4a9d0b3b4ed8389158379
Babban33 <chaitanyawankhede705@gmail.com>
2024-05-11 12:23:57 +0530

    update: added vspm logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Birac from './birac.png';
 import Betic from './betic.jpg';
-// import SDK from './sdk.png';
+import SDK from './sdk.png';
 import College from './college.png';
 
 function NavBar() {
@@ -9,7 +9,7 @@ function NavBar() {
     <div className="flex justify-center -mb-16">
       <div className='flex items-center space-x-4 p-4'>
         <img src={Betic} alt="Betic Logo" className="h-16" />
-        {/* <img src={SDK} alt="SDK Logo" className="h-16" /> */}
+        <img src={SDK} alt="SDK Logo" className="h-16" />
         <img src={Birac} alt="BIRAC Logo" className="h-16" />
         <img src={College} alt="GHRCE Logo" className='h-16' />
       </div>
