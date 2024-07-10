dd8014225269b9daedda06c58e48b447084a6f29
Babban33 <chaitanyawankhede705@gmail.com>
2024-07-10 13:31:21 +0530

    ls: fetched osmf

--- src/Pages/Result/results.jsx
+++ src/Pages/Result/results.jsx
@@ -6,7 +6,16 @@ function Results({ disease1, disease2, disease3, disease4, disease5}) {
   const [phenotype, setDisease3] = useState(null);
   const [calculus, setDisease4] = useState(null);
   const [caries, setDisease5] = useState(null);
+  const [osmfPred, setOsmfPred] = useState([]);
+  const [osmfImages, setOsmfImages] = useState(false);
 
+  useEffect(()=>{
+    const storedImagePairs = JSON.parse(localStorage.getItem('osmf')) || [];
+    setOsmfPred(storedImagePairs);
+    if(osmfPred.length > 0){
+      setOsmfImages(true);
+    }
+  }, []);
   // Update disease state when disease1 prop changes
   useEffect(() => {
     setDisease1(disease1);
@@ -20,6 +29,25 @@ function Results({ disease1, disease2, disease3, disease4, disease5}) {
     <div className='space-y-6 text-gray-800'>
       <h1>Results</h1>
       <p>OSMF Screening: {osmf ? osmf : 'Screening not done'}</p>
+      {osmfImages &&(
+        <div className='grid grid-cols-3 gap-2'>
+          {osmfPred.map((pair, index)=>{
+            <div key={index} className="border border-gray-300 rounded-md p-4">
+              <h2 className="text-lg font-semibold">Image Pair {index + 1}</h2>
+              <div className="mt-2">
+                <img src={pair.capturedPhoto} alt="Captured" className="rounded-lg shadow-md" />
+              </div>
+              <div className="mt-2">
+                <img src={`data:image/jpeg;base64,${pair.generatedImage}`} alt="Generated" className="rounded-lg shadow-md" />
+              </div>
+              <div className="mt-4">
+                <p>Predicted Class: {pair.prediction}</p>
+                <p>Confidence: {pair.confidence}</p>
+              </div>
+            </div>
+          })}
+        </div>
+      )}
       <p>Gingivitis Screening: {gingivitis ? gingivitis : 'Screening not done'}</p>
       <p>Phenotype Screening: {phenotype ? phenotype : 'Screening not done'}</p>
       <p>Calculus Screening: {calculus ? calculus : 'Screening not done'}</p>
