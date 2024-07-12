1de46a67fb464bbab95740b537764f99c4fd0a47
Babban33 <chaitanyawankhede705@gmail.com>
2024-07-12 15:33:25 +0530

    res: added bg color and text-white

--- src/Pages/Result/results.jsx
+++ src/Pages/Result/results.jsx
@@ -33,48 +33,48 @@ function Results({ disease1, disease2, disease3, disease4, disease5 }) {
       <h1 className='font-serif text-4xl font-bold text-indigo-600 leading-tight text-center mb-8'>Screening Report</h1>
       
       <div className='border border-gray-300 rounded-md p-4 mb-8'>
-        <h2 className='font-serif text-2xl font-semibold text-indigo-600 leading-tight mb-4'>Patient Information</h2>
+        <h2 className='font-serif text-2xl font-semibold text-white bg-indigo-800 leading-tight mb-4 py-2'>Patient Information</h2>
           <p><strong>Name:</strong> {patientInfo.name || 'N/A'}</p>
           <p><strong>Age:</strong> {patientInfo.age || 'N/A'}</p>
           <p><strong>Gender:</strong> {patientInfo.gender || 'N/A'}</p>
           <p><strong>Village:</strong> {patientInfo.village || 'N/A'}</p>
       
-      <div className='p-4 mb-8'>
-        <h2 className='font-serif text-2xl font-semibold text-indigo-600 leading-tight mb-4'>OSMF</h2>
-        {osmfPairs.length > 0 ? (
-          <>
-            <div className='flex flex-row justify-between mb-4'>
-              <div className='basis-1/3 font-medium text-center'>Clicked Photo</div>
-              <div className='basis-1/3 font-medium text-center'>Predicted Image</div>
-              <div className='basis-1/3 font-medium text-center'>Prediction</div>
-            </div>
-            {osmfPairs.map((pair, index) => (
-              <div key={index} className="flex flex-row gap-4 mb-4 items-center">
-                <div className='basis-1/3'>
-                  <img src={pair.capturedPhoto} alt="Captured" className="rounded-lg shadow-md w-full" />
-                </div>
-                <div className='basis-1/3'>
-                  <img src={`data:image/jpeg;base64,${pair.generatedImage}`} alt="Generated" className="rounded-lg shadow-md w-full" />
-                </div>
-                <div className='basis-1/3 text-center'>
-                  <p><strong>Class:</strong> {pair.prediction}</p>
-                  <p><strong>Confidence:</strong> {pair.confidence}</p>
-                </div>
+        <div className='p-4 mb-8'>
+          <h2 className='font-serif text-2xl font-semibold text-white leading-tight mb-4 bg-indigo-800 p-2'>OSMF</h2>
+          {osmfPairs.length > 0 ? (
+            <>
+              <div className='flex flex-row justify-between mb-4'>
+                <div className='basis-1/2 font-medium text-center'>Clicked Photo</div>
+                <div className='basis-1/2 font-medium text-center'>Predicted Image</div>
+                <div className='basis-1/5 font-medium text-center'>Prediction</div>
               </div>
-            ))}
-          </>
-        ) : (
-          <h3 className='font-medium'>Screening Not Done.</h3>
-        )}
-      </div>
+              {osmfPairs.map((pair, index) => (
+                <div key={index} className="flex flex-row gap-4 mb-4 items-center">
+                  <div className='basis-1/2'>
+                    <img src={pair.capturedPhoto} alt="Captured" className="rounded-lg shadow-md w-full" />
+                  </div>
+                  <div className='basis-1/2'>
+                    <img src={`data:image/jpeg;base64,${pair.generatedImage}`} alt="Generated" className="rounded-lg shadow-md w-full" />
+                  </div>
+                  <div className='basis-1/5 text-center'>
+                    <p><strong>Class:</strong> {pair.prediction}</p>
+                    <p><strong>Confidence:</strong> {pair.confidence}</p>
+                  </div>
+                </div>
+              ))}
+            </>
+          ) : (
+            <h3 className='font-medium'>Screening Not Done.</h3>
+          )}
+        </div>
 
-      <div className='p-4'>
-        <h2 className='font-serif text-2xl font-semibold text-indigo-600 leading-tight mb-4'>Other Screenings</h2>
-        <p className='mb-2'><strong>Gingivitis Screening:</strong> {gingivitis || 'Screening not done'}</p>
-        <p className='mb-2'><strong>Phenotype Screening:</strong> {phenotype || 'Screening not done'}</p>
-        <p className='mb-2'><strong>Calculus Screening:</strong> {calculus || 'Screening not done'}</p>
-        <p className='mb-2'><strong>Caries Screening:</strong> {caries || 'Screening not done'}</p>
-      </div>
+        <div className='p-4'>
+          <h2 className='font-serif text-2xl font-semibold text-indigo-600 leading-tight mb-4'>Other Screenings</h2>
+          <p className='mb-2'><strong>Gingivitis Screening:</strong> {gingivitis || 'Screening not done'}</p>
+          <p className='mb-2'><strong>Phenotype Screening:</strong> {phenotype || 'Screening not done'}</p>
+          <p className='mb-2'><strong>Calculus Screening:</strong> {calculus || 'Screening not done'}</p>
+          <p className='mb-2'><strong>Caries Screening:</strong> {caries || 'Screening not done'}</p>
+        </div>
       </div>
     </div>
   );
