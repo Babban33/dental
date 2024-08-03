726d97144cd8754ee27f00208ec988db968c2b31
Babban33 <chaitanyawankhede705@gmail.com>
2024-08-03 14:26:53 +0530

    added multilingual

--- src/Pages/Options/options.jsx
+++ src/Pages/Options/options.jsx
@@ -1,16 +1,26 @@
 import { useNavigate } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import content from "../data.json";
 
 function SelectionPage(){
     const navigate = useNavigate();
+    const [language, setLanguage] = useState('en');
+
+    useEffect(()=>{
+        const getLang= ()=>{
+            setLanguage(localStorage.getItem('lang'));
+        }
+        getLang();
+    })
 
     return(
         <div className='space-y-6'>
-            <h1 className="font-serif text-4xl font-bold text-indigo-600 leading-tight">Selection Page</h1>
-            <button onClick={() => navigate('/osmf')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>OSMF</button>
-            <button onClick={() => navigate('/gingivitis')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>Gingivitis</button>
-            <button onClick={() => navigate('/phenotype')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>Gingival Phenotype</button>
-            <button onClick={() => navigate('/calculus')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>Calculus</button>
-            <button onClick={() => navigate('/caries')} className='bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>Caries</button>
+            <h1 className="font-serif text-4xl font-bold text-indigo-600 leading-tight">{content["Select"][language].title}</h1>
+            <button onClick={() => navigate('/osmf')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis1}</button>
+            <button onClick={() => navigate('/gingivitis')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis2}</button>
+            <button onClick={() => navigate('/phenotype')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis3}</button>
+            <button onClick={() => navigate('/calculus')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis4}</button>
+            <button onClick={() => navigate('/caries')} className='bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis5}</button>
         </div>
     )
 }
