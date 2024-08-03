3b432c913571567a7b07cd614d2f78f7effb9e76
Babban33 <chaitanyawankhede705@gmail.com>
2024-08-03 12:21:22 +0530

    added multilingual

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavButton from "../../components/btn";
+import content from "../data.json";
 function HomePage() {
+  const [language, setLanguage] = useState(localStorage.getItem('lang') || 'en');
+  
+  useEffect(()=>{
+    localStorage.setItem('lang', language);
+  }, [language]);
+
+  const handleLanguageChange = (event) => {
+    const newLanguage = event.target.value;
+    setLanguage(newLanguage);
+    localStorage.setItem('language', newLanguage);
+  };
+
   return (
     <div className="text-center p-8 mx-4 w-3/4">
       <h1 className="font-serif text-4xl font-bold text-indigo-600 leading-tight">
-        BIRAC AGC JanCare Project
+        {content["HomePage"][language].title}
       </h1>
 
       <h2 className="font-sans text-2xl font-medium text-gray-700 leading-snug">
-        Smart Web Based Application for Dental Disease Detection using Artificial Intelligence
+        {content["HomePage"][language].subtitle}
       </h2>
 
       <p className="font-sans text-gray-500 text-lg italic mt-2">
-        Smile with Confidence
+        {content["HomePage"][language].tagline}
       </p>
 
-      <NavButton text="Let's get started" destination="/info"/>
+      <div className="flex items-center justify-center mt-4 space-x-4">
+        <NavButton text={content["HomePage"][language].buttonText} destination="/info" />
+        <select
+          className="border border-gray-300 rounded-md p-2 bg-white text-gray-700"
+          value={language}
+          onChange={handleLanguageChange}
+        >
+          <option value="en">English</option>
+          <option value="hi">Hindi</option>
+          <option value="mar">Marathi</option>
+        </select>
+      </div>
     </div>
   );
 }
