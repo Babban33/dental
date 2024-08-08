591a58fd82a0be2b98375b2b88280dfedb00b265
Babban33 <chaitanyawankhede705@gmail.com>
2024-08-08 21:08:27 +0530

    added mobile layout

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,15 +3,18 @@ import Birac from './birac.png';
 import Betic from './betic.jpg';
 import SDK from './sdk.png';
 import College from './college.png';
+import { isMobile } from 'react-device-detect';
 
 function NavBar() {
+  const logoSize = isMobile ? 'h-10' : 'h-16';
+
   return (
     <div className="flex justify-center items-center -mb-16">
       <div className='flex items-center space-x-4 p-4'>
-        <img src={College} alt="GHRCE Logo" className='h-16' />
-        <img src={Birac} alt="BIRAC Logo" className="h-16" />
-        <img src={SDK} alt="SDK Logo" className="h-16" />
-        <img src={Betic} alt="Betic Logo" className="h-16" />
+        <img src={College} alt="GHRCE Logo" className={logoSize} />
+        <img src={Birac} alt="BIRAC Logo" className={logoSize} />
+        <img src={SDK} alt="SDK Logo" className={logoSize} />
+        <img src={Betic} alt="Betic Logo" className={logoSize} />
       </div>
     </div>
   );
