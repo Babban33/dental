3b3572a33d5880d266e7a1cabf5cc451ccaf0137
Babban33 <chaitanyawankhede705@gmail.com>
2024-08-16 11:58:04 +0530

    added margin

--- src/Pages/Options/options.jsx
+++ src/Pages/Options/options.jsx
@@ -17,14 +17,14 @@ function SelectionPage(){
         <div className='space-y-6'>
             <h1 className="font-serif text-4xl font-bold text-indigo-600 leading-tight">{content["Select"][language].title}</h1>
             <BrowserView>
-                <button onClick={() => navigate('/osmf')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis1}</button>
-                <button onClick={() => navigate('/gingivitis')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis2}</button>
-                <button onClick={() => navigate('/phenotype')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis3}</button>
-                <button onClick={() => navigate('/calculus')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis4}</button>
-                <button onClick={() => navigate('/caries')} className='bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis5}</button>
+                <button onClick={() => navigate('/osmf')} className='m-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis1}</button>
+                <button onClick={() => navigate('/gingivitis')} className='m-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis2}</button>
+                <button onClick={() => navigate('/phenotype')} className='m-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis3}</button>
+                <button onClick={() => navigate('/calculus')} className='m-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis4}</button>
+                <button onClick={() => navigate('/caries')} className='m-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis5}</button>
             </BrowserView>
             <MobileView>
-                <button onClick={() => navigate('/osmf')} className='mr-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis1}</button>
+                <button onClick={() => navigate('/osmf')} className='m-2 bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-3 px-6 rounded-full'>{content["Select"][language].dis1}</button>
             </MobileView>
         </div>
     )
